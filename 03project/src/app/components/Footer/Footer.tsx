11c92847b5a63364b5ad91ce0613eb5a0d09b870
IFTIKHARHUSSAINBUKHARI <iftikharhussainbukhari@gmail.com>
2024-10-24 00:15:46 +0500

    classwork done

---
+++ 03project/src/app/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-300 py-4 mt-8">
+      <div className="container text-center">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi dolore, non cupiditate molestias dolorum temporibus, impedit in possimus ratione ipsum consequuntur omnis magni unde reiciendis id. Odio voluptate facere amet?</p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
