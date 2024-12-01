8fe0bbc617cd75de250de0903a9f07337e64d25e
IFTIKHARHUSSAINBUKHARI <iftikharhussainbukhari@gmail.com>
2024-12-01 14:31:52 +0500

    6 project complete

---
+++ 6project/src/app/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Header from "../components/Header/Header";
+import Navbar from "@/components/Navbar/Navbar";
+import Hero from "@/components/Hero/Hero";
+import Company from "@/components/Company/Company";
+import Course from "@/components/Course/Course";
+import Achievement from "@/components/Achievement/Achievement";
+import Detail from "@/components/Detail/Detail";
+import Team from "@/components/Team/Team";
+import Testimonials from "@/components/Testimonial/Testimonial";
+import Footer from "@/components/Footer/Footer";
+
+function Home() {
+  return (
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Company></Company>
+      <Course></Course>
+      <Achievement></Achievement>
+      <Detail></Detail>
+      <Team></Team>
+      <Testimonials></Testimonials>
+      <Footer></Footer>
+    </div>
+  );
+}
+export default Home;
